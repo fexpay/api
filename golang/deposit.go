c303a7046e189d6f0b2c41659c6660eb60f46973
cheng jiangang <cheng.jiangang@qq.com>
2018-07-17 12:19:51 +0800

    增加golang示例

---
+++ golang/deposit.go
@@ -0,0 +1,133 @@
+package main 
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/des"
+	"encoding/json"
+	"encoding/hex"
+	"net/url"
+	"fmt"
+	"strings"
+	"github.com/axgle/mahonia"
+)
+
+//DES加密
+func DesEncrypt(originData, key[]byte) (string, error) {
+	var enc mahonia.Encoder
+	
+	enc = mahonia.NewEncoder("gbk")
+	
+	if enc == nil {
+		
+		return "", nil
+	}
+	originDataStr := enc.ConvertString(string(originData))
+	originData = []byte(originDataStr)
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	
+	originData = PKCS5Padding(originData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key)
+	crypted := make([]byte, len(originData))
+	blockMode.CryptBlocks(crypted, originData)
+	encodeString := hex.EncodeToString(crypted)
+	
+	return encodeString, nil
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext) % blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func DesDecrypt(encodeString string, key []byte)(string, error) {
+	var dec mahonia.Decoder
+	crypted, err := hex.DecodeString(encodeString)
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	blockMode := cipher.NewCBCDecrypter(block, key)
+	originData := crypted
+	blockMode.CryptBlocks(originData, crypted)
+	originData = ZeroUnPadding(originData)
+	dec = mahonia.NewDecoder("gbk")
+	originDataStr := dec.ConvertString(string(originData))
+
+	return originDataStr, nil
+}
+
+func ZeroUnPadding(originData []byte) []byte {
+	length := len(originData)
+	unpadding := int(originData[length - 1])
+
+	return originData[:(length - unpadding)];
+}
+
+func Md5(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+} 
+
+type DataOfParameters struct {
+	RealName 	string   `json:"realName"`
+	Amount	 	string   `json:"amount"`
+	OrderId  	string   `json:"orderId"`
+	IdCard   	string   `json:"idCard"`
+	NotifyUrl 	string   `json:"notifyUrl"`
+	ReturnUrl 	string   `json:"returnUrl"`
+	SendTime	string   `json:"sendTime"`	
+}
+
+type InterfaceParameters struct {
+	Data DataOfParameters	`json:"data"`
+	Sign string 			`json:"sign"`
+}
+
+func main() {
+	key := "83A6AEDE-3B5A-4D3E-B789-DC780421C1A1"
+	// smac
+	smac := "C628C57D3F0FCB7652D9C5D64898DFFF"
+
+	/**
+ 	 * 入金数据加密过程
+ 	*/
+	// 接口参数次序如下
+	data := DataOfParameters{}
+	data.RealName = url.QueryEscape("实名02") 	// 客户真实姓名，可空
+	data.Amount = "1000"						// 入金金额(美元)
+	data.OrderId = "2018071318275278924"		// 商家平台入金订单ID
+	data.IdCard = "2222222222"					// 2222222222 客户证件号码，可空
+	data.NotifyUrl = "https://www.domain.com/notify/callback" 	// 入金成功后，定时 同步Url
+	data.ReturnUrl = "https://www.domain.com/return/callback"	// 入金成功后，立即 调用Url
+	data.SendTime = "2018-07-13 18:27:52"		// 入金时间
+
+	marshalData, _ := json.Marshal(data)
+	
+	params := InterfaceParameters{}
+	params.Data = data
+	params.Sign = strings.ToUpper(Md5(string(marshalData) + smac))
+	//s = string([]rune(s)[:3])
+	key = strings.ToUpper(Md5(key))
+	key = string([]byte(key)[:8])
+
+    cipherStr, _ := json.Marshal(params)
+
+    fmt.Println("入金数据加密")
+    fmt.Println("原文:" + string(cipherStr))
+
+	// DES CBC 加解密
+    encryptedData, _ := DesEncrypt(cipherStr, []byte(key))
+
+    fmt.Println("密文:" + strings.ToUpper(encryptedData))
+
+    decryptedData, _ := DesDecrypt(encryptedData, []byte(key))
+    fmt.Println("解密:" + decryptedData)
+}
